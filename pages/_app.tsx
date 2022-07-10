7adc2376636198df72549ed91fe54b6419fe00dd
ss-dimasm <dimas.m@softwareseni.com>
2022-07-10 09:10:08 +0700

    feat: add core next.js pages

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import { ThemeProvider } from 'styled-components';
+import { themeConfig } from '../core/theme';
+import Head from 'next/head';
+import { Container } from '../components/atomic/container';
+import { FlexBox } from '../components/atomic/flex-box';
+import BottomNavigator from '../components/pages/bottom-navigator';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <ThemeProvider theme={themeConfig}>
+      <Head>
+        <title key='local'>@dimasbaguspm - personal site</title>
+      </Head>
+      <Container>
+        <FlexBox justifyCenter alignCenter flexColumn>
+          <Component {...pageProps} />
+          <BottomNavigator />
+        </FlexBox>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
-export default MyApp
+export default MyApp;
